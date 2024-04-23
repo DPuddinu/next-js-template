caf42c3c7b18b4dcfdcf176fdec836d9d9ab36f8
Dario Puddinu <dpuddinu.dev@gmail.com>
2024-04-23 11:51:10 +0200

    chore: update layout and initial page

--- src/config/nav.ts
+++ src/config/nav.ts
@@ -1,32 +1,7 @@
 import { SidebarLink } from '@/components/SidebarItems';
-import { Cog, Globe, HomeIcon } from 'lucide-react';
-
-type AdditionalLinks = {
-  title: string;
-  links: SidebarLink[];
-};
+import { Cog, HomeIcon } from 'lucide-react';
 
 export const defaultLinks: SidebarLink[] = [
-  { href: '/dashboard', title: 'Home', icon: HomeIcon },
-  { href: '/settings', title: 'Settings', icon: Cog },
-  { href: '/explore', title: 'Explore', icon: Globe },
-  { href: '/templates', title: 'Templates', icon: Globe }
-];
-
-export const additionalLinks: AdditionalLinks[] = [
-  {
-    title: 'Entities',
-    links: [
-      {
-        href: '/template-entries',
-        title: 'Template Entries',
-        icon: Globe
-      },
-      {
-        href: '/templates',
-        title: 'Templates',
-        icon: Globe
-      }
-    ]
-  }
+  { href: '/', title: 'Home', icon: HomeIcon },
+  { href: '/settings', title: 'Settings', icon: Cog }
 ];
